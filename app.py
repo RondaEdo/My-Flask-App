86d1df010d16dfa1df07d7d4c0814682a97b78de
Ronda <johnronda0527@gmail.com>
2026-03-08 21:19:50 +0800

    installed flask

--- app.py
+++ app.py
@@ -1,3 +1,16 @@
 from flask import Flask
 from flask_scss import Scss
 from flask_sqlalchemy import SQLAlchemy
+
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+        return "Testing 123"
+
+
+if __name__ in "__main__":
+        app.run(debug=True)
